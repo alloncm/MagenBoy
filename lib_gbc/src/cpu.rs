942a496cfff146d1e415f784789cd4ad3791f0f4
alloncm <alloncm@gmail.com>
2020-01-10 01:26:29 +0200

    added more register interfaces for cpu and fixed the tests

--- lib_gbc/src/cpu.rs
+++ lib_gbc/src/cpu.rs
@@ -1,28 +1,67 @@
-
-mod cpu
+pub struct GbcCpu
 {
-    pub struct GbcCpu
+    pub a:u8,
+    pub f:u8,
+    pub b:u8,
+    pub c:u8,
+    pub d:u8,
+    pub e:u8,
+    pub h:u8,
+    pub l:u8,
+    pub stack_pointer:u16,
+    pub program_counter:u16
+}
+
+impl GbcCpu
+{
+    pub fn af(&self)->u16
     {
-        pub a:u8,
-        pub f:u8,
-        pub b:u8,
-        pub c:u8,
-        pub d:u8,
-        pub e:u8,
-        pub h:u8,
-        pub l:u8,
-        pub stack_pointer:u16,
-        pub program_counter:u16
+        let mut value:u16 = self.a as u16;
+        value<<=8;
+        value+=self.f as u16;
+        return value;
     }
 
-    impl GbcCpu
+    pub fn bc(&self)->u16
     {
-        pub fn af(&self)->u16
-        {
-            let mut value:u16 = self.a as u16;
-            value<<=8;
-            value+=self.f as u16;
-            return value;
+        let mut value:u16 = self.b as u16;
+        value<<=8;
+        value+=self.c as u16;
+        return value;
+    }
+
+    pub fn de(&self)->u16
+    {
+        let mut value:u16 = self.d as u16;
+        value<<=8;
+        value+=self.e as u16;
+        return value;
+    }
+    
+    pub fn hl(&self)->u16
+    {
+        let mut value:u16 = self.h as u16;
+        value<<=8;
+        value+=self.l as u16;
+        return value;
+    }
+}   
+
+impl Default for GbcCpu
+{
+    fn default()->GbcCpu
+    {
+        GbcCpu{
+            a:0,
+            f:0,
+            b:0,
+            c:0,
+            d:0,
+            e:0,
+            h:0,
+            l:0,
+            stack_pointer:0,
+            program_counter:0
         }
     }
 }
--- lib_gbc/tests/gbc_cpu_tests.rs
+++ lib_gbc/tests/gbc_cpu_tests.rs
@@ -1,13 +1,12 @@
 
-use lib_gbc::cpu;
-
-fn make_cpu()->cpu::GbcCpu
-{
-
-}
+extern crate lib_gbc;
+use lib_gbc::cpu::GbcCpu;
 
 #[test]
 fn test_af()
 {
-    assert_eq!(6,6)
+    let mut cpu:GbcCpu = GbcCpu::default();
+    cpu.a = 0x5;
+    cpu.f = 0x6;
+    assert_eq!(cpu.af(),0x0506);
 }
