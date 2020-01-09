060a9199a8ccf63bccaaeb251ab07f3509f1e461
alloncm <alloncm@gmail.com>
2020-01-10 01:28:46 +0200

    added tests for the registers functions

--- lib_gbc/tests/gbc_cpu_tests.rs
+++ lib_gbc/tests/gbc_cpu_tests.rs
@@ -10,3 +10,30 @@ fn test_af()
     cpu.f = 0x6;
     assert_eq!(cpu.af(),0x0506);
 }
+
+#[test]
+fn test_bc()
+{
+    let mut cpu:GbcCpu = GbcCpu::default();
+    cpu.b = 0x5;
+    cpu.c = 0x6;
+    assert_eq!(cpu.bc(),0x0506);
+}
+
+#[test]
+fn test_de()
+{
+    let mut cpu:GbcCpu = GbcCpu::default();
+    cpu.d = 0x5;
+    cpu.e = 0x6;
+    assert_eq!(cpu.de(),0x0506);
+}
+
+#[test]
+fn test_hl()
+{
+    let mut cpu:GbcCpu = GbcCpu::default();
+    cpu.h = 0x5;
+    cpu.l = 0x6;
+    assert_eq!(cpu.hl(),0x0506);
+}
