39bf33ad1da887d6bcac2507bb90ff834f546309
alloncm <alloncm@gmail.com>
2020-02-15 18:29:14 +0200

    finished the load 16 bit commands

--- lib_gbc/src/opcodes/load_16bit_instructions.rs
+++ lib_gbc/src/opcodes/load_16bit_instructions.rs
@@ -22,7 +22,7 @@ pub fn load_sp_hl(cpu:&mut GbcCpu){
 }
 
 //pop from the stack pointer to one register
-pub fn pop(cpu:&mut GbcCpu, memory:&dyn Memory, opcode:u8){
+pub fn pop(cpu:&mut GbcCpu, memory:&mut dyn Memory, opcode:u8){
     let reg = opcode&0xF0;
     let reg = match reg{
         0xC=>&mut cpu.bc,
@@ -35,4 +35,35 @@ pub fn pop(cpu:&mut GbcCpu, memory:&dyn Memory, opcode:u8){
     *reg.high() = memory.read(cpu.stack_pointer);
     *reg.low() = memory.read(cpu.stack_pointer+1);
     cpu.stack_pointer+=2;
+}
+
+//push to stack the register 
+pub fn push(cpu:&mut GbcCpu, memory:&mut dyn Memory, opcode:u8){
+    let reg = opcode&0xF0;
+    let reg = match reg{
+        0xC=>&mut cpu.bc,
+        0xD=>&mut cpu.de,
+        0xE=>&mut cpu.hl,
+        0xF=>&mut cpu.af,
+        _=>panic!("no register")
+    };
+
+    memory.write(cpu.stack_pointer, *reg.high());
+    memory.write(cpu.stack_pointer-1, *reg.low());
+    cpu.stack_pointer-=2;
+}
+
+//load into hl sp + rr
+pub fn ld_hl_spnn(cpu:&mut GbcCpu, opcode:u16){
+    let value = opcode & 0xFF;
+    cpu.hl.value = cpu.stack_pointer + value;
+}
+
+//load sp into memory
+pub fn ld_nn_sp(cpu:&mut GbcCpu, memory:&mut dyn Memory, opcode:u32){
+    let address = (opcode & 0xFFFF) as u16;
+    let low = (cpu.stack_pointer & 0xFF) as u8;
+    let high = ((cpu.stack_pointer & 0xFF)>>8) as u8;
+    memory.write(address, low);
+    memory.write(address, high);
 }
