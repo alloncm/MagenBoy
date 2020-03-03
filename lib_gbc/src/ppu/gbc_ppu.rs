e9725bb1ea6cc14b6acb1f5a39e6dedac959397a
alloncm <alloncm@gmail.com>
2020-03-04 00:00:11 +0200

    starting to implement the buffer func

--- lib_gbc/src/ppu/gbc_ppu.rs
+++ lib_gbc/src/ppu/gbc_ppu.rs
@@ -3,7 +3,14 @@ use crate::machine::memory::Memory;
 
 const SCREEN_HEIGHT: usize = 144;
 const SCREEN_WIDTH: usize = 160;
-const SCREEN_BUFFER_SIZE: usize = 0xFF*0xFF;
+const SCREEN_BUFFER_SIZE: usize = 0xFFFF;
+const VRAM_START_ADDRESS:u16 = 0x8000;
+const VRAM_END_ADDRESS:u16 = 0x97FF;
+const SPRITE_NORMAL_SIZE:u8 = 8;
+
+struct Sprite{
+    pixels:[u8;64]
+}
 
 pub struct GbcPpu<'a>{
     pub screen_cordinates: Vec2<u8>,
@@ -17,7 +24,20 @@ pub struct GbcPpu<'a>{
 }
 
 impl<'a> GbcPpu<'a>{
-    pub fn get_screen_buffer(&self)->[u8;SCREEN_HEIGHT*SCREEN_WIDTH]{
-        return [0;23040];
+    pub fn get_screen_buffer(&self)->[u8;SCREEN_BUFFER_SIZE]{
+        let sprite_size = SPRITE_NORMAL_SIZE + (SPRITE_NORMAL_SIZE * self.sprite_extended as u8);
+        let mut screen:[u8;SCREEN_BUFFER_SIZE] = [0;SCREEN_BUFFER_SIZE];
+        for i in (VRAM_START_ADDRESS..=VRAM_END_ADDRESS).step_by(2){
+            let screen_index:u16 = i-VRAM_START_ADDRESS;
+            let first_byte:u8 = self.memory.read(i);
+            let second_byte:u8 = self.memory.read(i+1);
+            for j in 0..8{
+                let mask:u8 = 1<<(7-j);
+                screen[(screen_index+j) as usize] = (first_byte & mask)>>7-j;
+                screen[(screen_index+j) as usize] |= ((second_byte & mask)>>7-j)<<1;
+            } 
+        }
+
+        return screen;
     }
 }
