1e45b9cd3055af48dffd787fe61337f112e9aad0
alloncm <alloncm@gmail.com>
2020-03-21 04:00:10 +0200

    added hram support

--- lib_gbc/src/machine/gbc_memory.rs
+++ lib_gbc/src/machine/gbc_memory.rs
@@ -4,11 +4,15 @@ use crate::machine::ram::Ram;
 use crate::machine::vram::VRam;
 use crate::machine::io_ports::IoPorts;
 
+const HRAM_SIZE:usize = 0x7E;
+
 pub struct GbcMmu{
     pub ram: Ram,
     pub vram: VRam,
     pub rom: Rom,
-    io_ports: IoPorts
+    io_ports: IoPorts,
+    hram: [u8;HRAM_SIZE],
+    interupt_enable_register:u8
 }
 
 
@@ -24,6 +28,8 @@ impl Memory for GbcMmu{
             0xD000..=0xDFFF=>self.ram.read_current_bank(address-0xD000),
             0xFEA0..=0xFEFF=>std::panic!("not useable"),
             0xFF00..=0xFF7F=>self.io_ports.memory[(address - 0xFF00) as usize],
+            0xFF80..=0xFFFE=>self.hram[(address-0xFF80) as usize],
+            0xFFFF=>self.interupt_enable_register,
             _=>std::panic!("not implemented yet")
         }
     }
@@ -35,7 +41,10 @@ impl Memory for GbcMmu{
             0xC000..=0xCFFF =>self.ram.write_bank0(address - 0xC000,value), 
             0xE000..=0xFDFF=>self.ram.write_bank0(address - 0xE000,value),
             0xD000..=0xDFFF=>self.ram.write_current_bank(address-0xD000,value),
+            0xFEA0..=0xFEFF=>std::panic!("not useable"),
             0xFF00..=0xFF7F=>self.io_ports.memory[(address - 0xFF00) as usize] = value,
+            0xFF80..=0xFFFE=>self.hram[(address-0xFF80) as usize] = value,
+            0xFFFF=>self.interupt_enable_register = value,
             _=>std::panic!("not implemented yet")
         }
     }
@@ -47,7 +56,9 @@ impl GbcMmu{
             ram:Ram::default(),
             io_ports:IoPorts::default(),
             rom:rom,
-            vram:VRam::default()
+            vram:VRam::default(),
+            hram:[0;HRAM_SIZE],
+            interupt_enable_register:0
         }
     }
 }
