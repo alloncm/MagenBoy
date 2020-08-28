8c21bac89cae63bfc6fd89b77e35feab6e5ac4b9
alloncm <alloncm@gmail.com>
2020-08-28 22:19:40 +0300

    added the reads vram and oam

--- lib_gbc/src/mmu/gbc_mmu.rs
+++ lib_gbc/src/mmu/gbc_mmu.rs
@@ -11,6 +11,8 @@ pub const BOOT_ROM_SIZE:usize = 0x100;
 const HRAM_SIZE:usize = 0x7F;
 const SPRITE_ATTRIBUTE_TABLE_SIZE:usize = 0xA0;
 
+const BAD_READ_VALUE:u8 = 0xFF;
+
 pub struct GbcMmu{
     pub ram: Ram,
     pub vram: VRam,
@@ -38,12 +40,26 @@ impl Memory for GbcMmu{
             },
             0x100..=0x3FFF=>self.mbc.read_bank0(address),
             0x4000..=0x7FFF=>self.mbc.read_current_bank(address-0x4000),
-            0x8000..=0x9FFF=>self.vram.read_current_bank(address-0x8000),
+            0x8000..=0x9FFF=>{
+                if self.is_vram_ready_for_io(){
+                    return self.vram.read_current_bank(address-0x8000);
+                }
+                else{
+                    return BAD_READ_VALUE;
+                }
+            },
             0xA000..=0xBFFF=>self.mbc.read_external_ram(address-0xA000),
             0xC000..=0xCFFF =>self.ram.read_bank0(address - 0xC000), 
             0xD000..=0xDFFF=>self.ram.read_current_bank(address-0xD000),
             0xE000..=0xFDFF=>self.ram.read_bank0(address - 0xE000),
-            0xFE00..=0xFE9F=>self.sprite_attribute_table[(address-0xFE00) as usize],
+            0xFE00..=0xFE9F=>{
+                if self.is_oam_ready_for_io(){
+                    return self.sprite_attribute_table[(address-0xFE00) as usize];
+                }
+                else{
+                    return BAD_READ_VALUE;
+                }
+            },
             0xFEA0..=0xFEFF=>0x0,
             0xFF00..=0xFF7F=>self.io_ports.read(address - 0xFF00),
             0xFF80..=0xFFFE=>self.hram[(address-0xFF80) as usize],
@@ -58,12 +74,20 @@ impl Memory for GbcMmu{
 
         match address{
             0x0..=0x7FFF=>self.mbc.write_rom(address, value),
-            0x8000..=0x9FFF=>self.vram.write_current_bank(address-0x8000, value),
+            0x8000..=0x9FFF=>{
+                if self.is_vram_ready_for_io(){
+                    self.vram.write_current_bank(address-0x8000, value);
+                }
+            },
             0xA000..=0xBFFF=>self.mbc.write_external_ram(address-0xA000,value),
             0xC000..=0xCFFF =>self.ram.write_bank0(address - 0xC000,value), 
             0xE000..=0xFDFF=>self.ram.write_bank0(address - 0xE000,value),
             0xD000..=0xDFFF=>self.ram.write_current_bank(address-0xD000,value),
-            0xFE00..=0xFE9F=>self.sprite_attribute_table[(address-0xFE00) as usize] = value,
+            0xFE00..=0xFE9F=>{
+                if self.is_oam_ready_for_io(){
+                    self.sprite_attribute_table[(address-0xFE00) as usize] = value;
+                }
+            },
             0xFEA0..=0xFEFF=>{},
             0xFF00..=0xFF7F=>self.io_ports.write(address - 0xFF00, value),
             0xFF80..=0xFFFE=>self.hram[(address-0xFF80) as usize] = value,
@@ -87,5 +111,14 @@ impl GbcMmu{
             finished_boot:false,
             ppu_state:PpuState::OamSearch
         }
+    }
+
+    fn is_oam_ready_for_io(&self)->bool{
+        let ppu_state = self.ppu_state as u8;
+        return ppu_state != PpuState::OamSearch as u8 && ppu_state != PpuState::PixelTransfer as u8
+    }
+
+    fn is_vram_ready_for_io(&self)->bool{
+        return self.ppu_state as u8 != PpuState::PixelTransfer as u8;
     }
 }
