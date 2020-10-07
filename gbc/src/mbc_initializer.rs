1002c4bed82f8fdad6c2934a4249577427cb1d6a
alloncm <alloncm@gmail.com>
2020-10-07 23:53:56 +0300

    fixed module visability and fixed typo

--- gbc/src/mbc_initializer.rs
+++ gbc/src/mbc_initializer.rs
@@ -1,7 +1,4 @@
-use lib_gbc::mmu::carts::mbc1::Mbc1;
-use lib_gbc::mmu::carts::mbc::Mbc;
-use lib_gbc::mmu::carts::rom::Rom;
-use lib_gbc::mmu::carts::mbc3::Mbc3;
+use lib_gbc::mmu::carts::*;
 use std::boxed::Box;
 use std::fs;
 use log::info;
@@ -13,7 +10,7 @@ pub const SAVE_SUFFIX:&str = ".sav";
 pub fn initialize_mbc(program_name:&String)->Box<dyn Mbc>{
 
     let program_path = format!("{}{}",program_name,PROGRAM_SUFFIX);
-    let program = fs::read(program_path).expect("No prgram found, notice that function must have a `.gb` suffix");
+    let program = fs::read(program_path).expect("No program found, notice that function must have a `.gb` suffix");
 
     let mbc_type = program[CARTRIDGE_TYPE_ADDRESS];
 
