79931ccdc358aa186a9abb05f8a55aa0bb8257b1
alloncm <alloncm@gmail.com>
2020-11-07 18:01:17 +0200

    Add the registers to the io ports

--- lib_gb/src/mmu/io_ports.rs
+++ lib_gb/src/mmu/io_ports.rs
@@ -5,11 +5,23 @@ const IO_PORTS_SIZE:usize = 0x80;
 
 const IO_PORTS_MEMORY_OFFSET:u16 = 0xFF00;
 
+macro_rules! io_port_index{
+    ($name:ident, $reg_address:expr) => {
+        const $name:u16 = $reg_address - IO_PORTS_MEMORY_OFFSET;
+    };
+}
+
 pub const DIV_REGISTER_INDEX:u16 = DIV_REGISTER_ADDRESS - IO_PORTS_MEMORY_OFFSET;
-const TAC_REGISTER_INDEX:u16 = TAC_REGISTER_ADDRESS - IO_PORTS_MEMORY_OFFSET;
-const STAT_REGISTER_INDEX:u16 = STAT_REGISTER_ADDRESS - IO_PORTS_MEMORY_OFFSET;
-const JOYP_REGISTER_INDEX:u16 = JOYP_REGISTER_ADDRESS - IO_PORTS_MEMORY_OFFSET;
-const DMA_REGISTER_INDEX:u16 = DMA_REGISTER_ADDRESS - IO_PORTS_MEMORY_OFFSET;
+
+io_port_index!(TAC_REGISTER_INDEX, TAC_REGISTER_ADDRESS);
+io_port_index!(STAT_REGISTER_INDEX, STAT_REGISTER_ADDRESS);
+io_port_index!(JOYP_REGISTER_INDEX, JOYP_REGISTER_ADDRESS);
+io_port_index!(DMA_REGISTER_INDEX, DMA_REGISTER_ADDRESS);
+io_port_index!(NR30_REGISTER_INDEX, NR30_REGISTER_ADDRESS);
+io_port_index!(NR31_REGISTER_INDEX, NR31_REGISTER_ADDRESS);
+io_port_index!(NR32_REGISTER_INDEX, NR32_REGISTER_ADDRESS);
+io_port_index!(NR33_REGISTER_INDEX, NR33_REGISTER_ADDRESS);
+io_port_index!(NR34_REGISTER_INDEX, NR34_REGISTER_ADDRESS);
 
 pub struct IoPorts{
     pub system_counter:u16,
@@ -19,7 +31,16 @@ pub struct IoPorts{
 
 impl IoPorts{
     pub fn read(&self, address:u16)->u8{
-        self.ports[address as usize]
+        let value = self.ports[address as usize];
+        match address{
+            NR30_REGISTER_INDEX=> value | 0x7F,
+            NR31_REGISTER_INDEX=> value | 0xFF,
+            NR32_REGISTER_INDEX=> value | 0x9F,
+            NR33_REGISTER_INDEX=> value | 0xFF,
+            NR34_REGISTER_INDEX=> value | 0xBF,
+            0x27..=0x2F=>0xFF,//Not used
+            _=>value
+        }
     }
 
     pub fn write(&mut self, address:u16, mut value:u8){
@@ -41,6 +62,7 @@ impl IoPorts{
                     0xA0..=0xFF=>Some(AccessBus::External)
                 }
             },
+            NR31_REGISTER_INDEX=>self.ports[NR31_REGISTER_INDEX as usize] = 0xFF,
             _=>{}
         }
 
