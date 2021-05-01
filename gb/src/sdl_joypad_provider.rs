8d974cc77e9ecb1d88e13f5c177d015399bc1eaa
alloncm <alloncm@gmail.com>
2021-05-01 19:06:59 +0300

    Move getting to keyboard state to the ctor

--- gb/src/sdl_joypad_provider.rs
+++ gb/src/sdl_joypad_provider.rs
@@ -10,13 +10,15 @@ const PUMP_THRESHOLD:u32 = 500;
 pub struct SdlJoypadProvider<F:Fn(Button)->SDL_Scancode>{
     mapper: F,
     pump_counter:u32,
+    keyborad_state: *const u8
 }
 
 impl<F:Fn(Button)->SDL_Scancode> SdlJoypadProvider<F>{
     pub fn new(mapper:F)->Self{
         SdlJoypadProvider{
             mapper,
-            pump_counter:PUMP_THRESHOLD
+            pump_counter:PUMP_THRESHOLD,
+            keyborad_state: unsafe{SDL_GetKeyboardState(std::ptr::null_mut())}
         }
     }
 }
@@ -30,16 +32,14 @@ impl<F:Fn(Button)->SDL_Scancode> JoypadProvider for SdlJoypadProvider<F>{
                 SDL_PumpEvents();
             }
 
-            let keyborad_state:*const u8 = SDL_GetKeyboardState(std::ptr::null_mut());
-
-            joypad.buttons[Button::A as usize]      = *keyborad_state.offset(mapper(Button::A) as isize) != 0;
-            joypad.buttons[Button::B as usize]      = *keyborad_state.offset(mapper(Button::B) as isize) != 0;
-            joypad.buttons[Button::Start as usize]  = *keyborad_state.offset(mapper(Button::Start) as isize) != 0;
-            joypad.buttons[Button::Select as usize] = *keyborad_state.offset(mapper(Button::Select) as isize) != 0;
-            joypad.buttons[Button::Up as usize]     = *keyborad_state.offset(mapper(Button::Up) as isize) != 0;
-            joypad.buttons[Button::Down as usize]   = *keyborad_state.offset(mapper(Button::Down) as isize) != 0;
-            joypad.buttons[Button::Right as usize]  = *keyborad_state.offset(mapper(Button::Right) as isize) != 0;
-            joypad.buttons[Button::Left as usize]   = *keyborad_state.offset(mapper(Button::Left) as isize) != 0;
+            joypad.buttons[Button::A as usize]      = *self.keyborad_state.offset(mapper(Button::A) as isize) != 0;
+            joypad.buttons[Button::B as usize]      = *self.keyborad_state.offset(mapper(Button::B) as isize) != 0;
+            joypad.buttons[Button::Start as usize]  = *self.keyborad_state.offset(mapper(Button::Start) as isize) != 0;
+            joypad.buttons[Button::Select as usize] = *self.keyborad_state.offset(mapper(Button::Select) as isize) != 0;
+            joypad.buttons[Button::Up as usize]     = *self.keyborad_state.offset(mapper(Button::Up) as isize) != 0;
+            joypad.buttons[Button::Down as usize]   = *self.keyborad_state.offset(mapper(Button::Down) as isize) != 0;
+            joypad.buttons[Button::Right as usize]  = *self.keyborad_state.offset(mapper(Button::Right) as isize) != 0;
+            joypad.buttons[Button::Left as usize]   = *self.keyborad_state.offset(mapper(Button::Left) as isize) != 0;
         }
     }
 }
