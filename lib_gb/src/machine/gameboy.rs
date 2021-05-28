46cdced3c31b752b2128cedb19eeaedaa0b1752c
alloncm <alloncm@gmail.com>
2021-05-28 14:55:24 +0300

    move the PPU to the IO comps

--- lib_gb/src/machine/gameboy.rs
+++ lib_gb/src/machine/gameboy.rs
@@ -1,4 +1,4 @@
-use crate::{apu::{self, audio_device::AudioDevice, gb_apu::GbApu}, cpu::{gb_cpu::GbCpu, opcodes::opcode_resolver::*}, keypad::{joypad::Joypad, joypad_provider::JoypadProvider, joypad_register_updater}, mmu::{carts::mbc::Mbc, gb_mmu::{GbMmu, BOOT_ROM_SIZE}, memory::{Memory, UnprotectedMemory}, mmu_register_updater, oam_dma_transferer::OamDmaTransferer}, ppu::{gb_ppu::{CYCLES_PER_FRAME, GbPpu, SCREEN_HEIGHT, SCREEN_WIDTH}, ppu_register_updater}, timer::{gb_timer::GbTimer, timer_register_updater}, utils::memory_registers::IF_REGISTER_ADDRESS};
+use crate::{apu::{audio_device::AudioDevice, gb_apu::GbApu}, cpu::{gb_cpu::GbCpu, opcodes::opcode_resolver::*}, keypad::{joypad::Joypad, joypad_provider::JoypadProvider, joypad_register_updater}, mmu::{carts::mbc::Mbc, gb_mmu::{GbMmu, BOOT_ROM_SIZE}, memory::Memory, mmu_register_updater, oam_dma_transferer::OamDmaTransferer}, ppu::{gb_ppu::{CYCLES_PER_FRAME, SCREEN_HEIGHT, SCREEN_WIDTH} }};
 use super::interrupts_handler::InterruptsHandler;
 use std::boxed::Box;
 use log::debug;
@@ -8,7 +8,6 @@ pub struct GameBoy<'a, JP: JoypadProvider, AD:AudioDevice> {
     cpu: GbCpu,
     mmu: GbMmu::<'a, AD>,
     opcode_resolver:OpcodeResolver::<GbMmu::<'a, AD>>,
-    ppu:GbPpu,
     interrupts_handler:InterruptsHandler,
     cycles_counter:u32, 
     joypad_provider: JP,
@@ -22,7 +21,6 @@ impl<'a, JP:JoypadProvider, AD:AudioDevice> GameBoy<'a, JP, AD>{
             cpu:GbCpu::default(),
             mmu:GbMmu::new_with_bootrom(mbc, boot_rom, GbApu::new(audio_device)),
             opcode_resolver:OpcodeResolver::default(),
-            ppu:GbPpu::default(),
             interrupts_handler: InterruptsHandler::default(),
             cycles_counter:0,
             joypad_provider: joypad_provider,
@@ -44,7 +42,6 @@ impl<'a, JP:JoypadProvider, AD:AudioDevice> GameBoy<'a, JP, AD>{
             cpu:cpu,
             mmu:GbMmu::new(mbc, GbApu::new(audio_device)),
             opcode_resolver:OpcodeResolver::default(),
-            ppu:GbPpu::default(),
             interrupts_handler: InterruptsHandler::default(),
             cycles_counter:0,
             joypad_provider: joypad_provider,
@@ -55,7 +52,7 @@ impl<'a, JP:JoypadProvider, AD:AudioDevice> GameBoy<'a, JP, AD>{
     pub fn cycle_frame(&mut self)->&[u32;SCREEN_HEIGHT*SCREEN_WIDTH]{
         let mut joypad = Joypad::default();
 
-        let mut last_ppu_power_state:bool = self.ppu.screen_enable;
+        let mut last_ppu_power_state:bool = self.mmu.io_comps.ppu.screen_enable;
 
         while self.cycles_counter < CYCLES_PER_FRAME{
             self.joypad_provider.provide(&mut joypad);
@@ -73,24 +70,20 @@ impl<'a, JP:JoypadProvider, AD:AudioDevice> GameBoy<'a, JP, AD>{
             self.dma.cycle(&mut self.mmu, cpu_cycles_passed as u8);
             
             
+            mmu_register_updater::update_mmu_registers(&mut self.mmu, &mut self.dma);
             self.mmu.io_comps.cycle(cpu_cycles_passed as u32);
 
             //For the PPU
-            mmu_register_updater::update_mmu_registers(&mut self.mmu, &mut self.dma);
 
-            ppu_register_updater::update_ppu_regsiters(&mut self.mmu, &mut self.ppu);
-            self.ppu.update_gb_screen(&mut self.mmu, cpu_cycles_passed as u32);
             mmu_register_updater::update_mmu_registers(&mut self.mmu, &mut self.dma);
             
             //interrupts
-            let interrupt_cycles = self.interrupts_handler.handle_interrupts(&mut self.cpu, &mut self.ppu, &mut self.mmu);
+            let interrupt_cycles = self.interrupts_handler.handle_interrupts(&mut self.cpu, &mut self.mmu);
             if interrupt_cycles != 0{
                 self.dma.cycle(&mut self.mmu, interrupt_cycles as u8);
                 mmu_register_updater::update_mmu_registers(&mut self.mmu, &mut self.dma);
                 
                 //PPU
-                ppu_register_updater::update_ppu_regsiters(&mut self.mmu, &mut self.ppu);
-                self.ppu.update_gb_screen(&mut self.mmu, interrupt_cycles as u32);
                 mmu_register_updater::update_mmu_registers(&mut self.mmu, &mut self.dma);
 
                 
@@ -100,28 +93,24 @@ impl<'a, JP:JoypadProvider, AD:AudioDevice> GameBoy<'a, JP, AD>{
             let iter_total_cycles= cpu_cycles_passed as u32 + interrupt_cycles as u32;
             
 
-            //APU
-            // apu::update_apu_registers(&mut self.mmu, &mut self.apu);
-            // self.apu.cycle(iter_total_cycles as u8);
-
             //clears io ports
             self.mmu.io_comps.ports.clear_io_ports_triggers();
 
             //In case the ppu just turned I want to keep it sync with the actual screen and thats why Im reseting the loop to finish
             //the frame when the ppu finishes the frame
-            if !last_ppu_power_state && self.ppu.screen_enable{
+            if !last_ppu_power_state && self.mmu.io_comps.ppu.screen_enable{
                 self.cycles_counter = 0;
             }
 
             self.cycles_counter += iter_total_cycles;
-            last_ppu_power_state = self.ppu.screen_enable;
+            last_ppu_power_state = self.mmu.io_comps.ppu.screen_enable;
         }
 
         if self.cycles_counter >= CYCLES_PER_FRAME{
             self.cycles_counter -= CYCLES_PER_FRAME; 
         }
 
-        return self.ppu.get_frame_buffer();
+        return self.mmu.io_comps.ppu.get_frame_buffer();
     }
 
     fn fetch_next_byte(&mut self)->u8{
