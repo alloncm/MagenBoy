391b362eae16fb7a358a99d43ffa0218e5e24be1
alloncm <alloncm@gmail.com>
2021-07-28 01:35:50 +0300

    Add the sprite fetcher

--- lib_gb/src/ppu/fifo/mod.rs
+++ lib_gb/src/ppu/fifo/mod.rs
@@ -2,3 +2,4 @@ pub mod fifo_ppu;
 pub mod fifo_register_updater;
 mod bg_fetcher;
 mod fetching_state;
+mod sprite_fetcher;
---
+++ lib_gb/src/ppu/fifo/sprite_fetcher.rs
@@ -0,0 +1,76 @@
+use std::mem::{self, MaybeUninit};
+
+use crate::{mmu::vram::VRam, ppu::sprite_attribute::SpriteAttribute};
+
+use super::fetching_state::FethcingState;
+
+pub struct SpriteFetcher{
+    pub fifo:Vec<(u8, u8)>,
+    pub oam_entries:[SpriteAttribute; 10],
+
+    current_fetching_state:FethcingState,
+    current_oam_entry:u8,
+}
+
+impl SpriteFetcher{
+    pub fn new()->Self{
+        let oam_entries = {
+            let mut data: [MaybeUninit<SpriteAttribute>; 10] = unsafe{
+                MaybeUninit::uninit().assume_init()
+            };
+
+            for elem in &mut data[..]{
+                *elem = MaybeUninit::new(SpriteAttribute::new(0, 0, 0, 0));
+            }
+
+            unsafe{mem::transmute::<_, [SpriteAttribute;10]>(data)}
+        };
+        
+        SpriteFetcher{
+            current_fetching_state:FethcingState::TileNumber,
+            current_oam_entry:0,
+            oam_entries,
+            fifo:Vec::<(u8,u8)>::with_capacity(8)
+        }
+    }
+
+    pub fn reset(&mut self){
+        self.current_oam_entry = 0;
+        self.current_fetching_state = FethcingState::TileNumber;
+        self.fifo.clear();
+    }
+
+    pub fn fetch_pixels(&mut self, vram:&VRam, ly_register:u8, lcd_control:u8){
+        match self.current_fetching_state{
+            FethcingState::TileNumber=>{
+                if self.oam_entries.len() > self.current_oam_entry as usize{
+                    self.current_fetching_state = FethcingState::LowTileData(self.oam_entries[self.current_oam_entry as usize].tile_number);
+                }
+            }
+            FethcingState::LowTileData(tile_num)=>{
+                let current_tile_data_address = 0x8000 + (tile_num as u16 * 16);
+                let low_data = vram.read_current_bank(current_tile_data_address + (2 * (ly_register % 8)) as u16);
+                self.current_fetching_state = FethcingState::HighTileData(tile_num, low_data);
+            }
+            FethcingState::HighTileData(tile_num, low_data)=>{
+                let current_tile_data_address = 0x8000 + (tile_num as u16 * 16);
+                let high_data = vram.read_current_bank(current_tile_data_address + (2 * (ly_register % 8)) as u16 + 1);
+                self.current_fetching_state = FethcingState::Push(low_data, high_data);
+            }
+            FethcingState::Push(low_data, high_data)=>{
+                let oam_attribute = &self.oam_entries[self.current_oam_entry as usize];
+                let range = if oam_attribute.flip_x {0..=7} else {7..=0};
+                for i in range{
+                    let mask = 1 << i;
+                    let mut pixel = (low_data & mask) >> i;
+                    pixel |= ((high_data & mask) >> i) << 1;
+                    self.fifo.push((pixel, self.current_oam_entry));
+                }
+
+                self.current_fetching_state = FethcingState::TileNumber;
+
+                self.current_oam_entry += 1;
+            }
+        }
+    }
+}
