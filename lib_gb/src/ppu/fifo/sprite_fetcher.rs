205f1fa482584cad2de4bdaa18924001ddce259b
alloncm <alloncm@gmail.com>
2021-08-13 14:01:17 +0300

    Trying to add support for extended sprites

--- lib_gb/src/ppu/fifo/sprite_fetcher.rs
+++ lib_gb/src/ppu/fifo/sprite_fetcher.rs
@@ -1,6 +1,6 @@
 use std::mem::{self, MaybeUninit};
 
-use crate::{mmu::vram::VRam, ppu::sprite_attribute::SpriteAttribute};
+use crate::{mmu::vram::VRam, ppu::sprite_attribute::SpriteAttribute, utils::bit_masks::{BIT_0_MASK, BIT_2_MASK}};
 
 use super::fetching_state::FethcingState;
 
@@ -46,13 +46,19 @@ impl SpriteFetcher{
         self.rendering = false;
     }
 
-    pub fn fetch_pixels(&mut self, vram:&VRam, ly_register:u8, current_x_pos:u8){
+    pub fn fetch_pixels(&mut self, vram:&VRam, lcd_control:u8, ly_register:u8, current_x_pos:u8){
+        let sprite_size = if lcd_control & & BIT_2_MASK == 0 {8} else{16};
+
         match self.current_fetching_state{
             FethcingState::TileNumber=>{
                 if self.oam_entries_len > self.current_oam_entry{
                     let oam_entry = &self.oam_entries[self.current_oam_entry as usize];
                     if oam_entry.x <= current_x_pos + 8 && current_x_pos < oam_entry.x{
-                        self.current_fetching_state = FethcingState::LowTileData(oam_entry.tile_number);
+                        let mut tile_number = oam_entry.tile_number;
+                        if lcd_control & BIT_2_MASK != 0{
+                            tile_number &= !BIT_0_MASK
+                        }
+                        self.current_fetching_state = FethcingState::LowTileData(tile_number);
                         self.rendering = true;
                         return;
                     }
@@ -63,9 +69,9 @@ impl SpriteFetcher{
             FethcingState::LowTileData(tile_num)=>{
                 let oam_attribute = &self.oam_entries[self.current_oam_entry as usize];
                 let current_tile_data_address = if !oam_attribute.flip_y {
-                    tile_num as u16 * 16 + (2 * (ly_register % 8)) as u16
+                    tile_num as u16 * 16 + (2 * (ly_register % sprite_size)) as u16
                 }else{
-                    tile_num as u16 * 16 + (2 * (7 - (ly_register % 8))) as u16
+                    tile_num as u16 * 16 + (2 * ((sprite_size - 1) - (ly_register % sprite_size))) as u16
                 };
                 let low_data = vram.read_current_bank(current_tile_data_address);
                 self.current_fetching_state = FethcingState::HighTileData(tile_num, low_data);
@@ -73,9 +79,9 @@ impl SpriteFetcher{
             FethcingState::HighTileData(tile_num, low_data)=>{
                 let oam_attribute = &self.oam_entries[self.current_oam_entry as usize];
                 let current_tile_data_address = if !oam_attribute.flip_y {
-                    tile_num as u16 * 16 + (2 * (ly_register % 8)) as u16
+                    tile_num as u16 * 16 + (2 * (ly_register % sprite_size)) as u16
                 }else{
-                    tile_num as u16 * 16 + (2 * (7 - (ly_register % 8))) as u16
+                    tile_num as u16 * 16 + (2 * ((sprite_size - 1) - (ly_register % sprite_size))) as u16
                 };
                 let high_data = vram.read_current_bank(current_tile_data_address + 1);
                 self.current_fetching_state = FethcingState::Push(low_data, high_data);
@@ -102,7 +108,6 @@ impl SpriteFetcher{
                 }
 
                 self.current_fetching_state = FethcingState::TileNumber;
-
                 self.current_oam_entry += 1;
             }
         }
