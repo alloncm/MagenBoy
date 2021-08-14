f2c4496acee4cf4d1e689417cf4fc14e5b2db178
alloncm <alloncm@gmail.com>
2021-08-15 00:10:41 +0300

    Implement the extended sprites correctly.
    Found a bug in the way I rendered the sprites.

--- lib_gb/src/ppu/fifo/sprite_fetcher.rs
+++ lib_gb/src/ppu/fifo/sprite_fetcher.rs
@@ -47,7 +47,7 @@ impl SpriteFetcher{
     }
 
     pub fn fetch_pixels(&mut self, vram:&VRam, lcd_control:u8, ly_register:u8, current_x_pos:u8){
-        let sprite_size = if lcd_control & & BIT_2_MASK == 0 {8} else{16};
+        let sprite_size = if lcd_control & BIT_2_MASK == 0 {8} else{16};
 
         match self.current_fetching_state{
             FethcingState::TileNumber=>{
@@ -68,21 +68,13 @@ impl SpriteFetcher{
             }
             FethcingState::LowTileData(tile_num)=>{
                 let oam_attribute = &self.oam_entries[self.current_oam_entry as usize];
-                let current_tile_data_address = if !oam_attribute.flip_y {
-                    tile_num as u16 * 16 + (2 * (ly_register % sprite_size)) as u16
-                }else{
-                    tile_num as u16 * 16 + (2 * ((sprite_size - 1) - (ly_register % sprite_size))) as u16
-                };
+                let current_tile_data_address = Self::get_current_tile_data_address(ly_register, oam_attribute, sprite_size, tile_num);
                 let low_data = vram.read_current_bank(current_tile_data_address);
                 self.current_fetching_state = FethcingState::HighTileData(tile_num, low_data);
             }
             FethcingState::HighTileData(tile_num, low_data)=>{
                 let oam_attribute = &self.oam_entries[self.current_oam_entry as usize];
-                let current_tile_data_address = if !oam_attribute.flip_y {
-                    tile_num as u16 * 16 + (2 * (ly_register % sprite_size)) as u16
-                }else{
-                    tile_num as u16 * 16 + (2 * ((sprite_size - 1) - (ly_register % sprite_size))) as u16
-                };
+                let current_tile_data_address = Self::get_current_tile_data_address(ly_register, oam_attribute, sprite_size, tile_num);
                 let high_data = vram.read_current_bank(current_tile_data_address + 1);
                 self.current_fetching_state = FethcingState::Push(low_data, high_data);
             }
@@ -111,5 +103,18 @@ impl SpriteFetcher{
                 self.current_oam_entry += 1;
             }
         }
+    }
+
+    // Receiving the tile_num since in case of extended sprite this could change (the first bit is reset)
+    fn get_current_tile_data_address(ly_register:u8, sprite_attrib:&SpriteAttribute, sprite_size:u8, tile_num:u8)->u16{
+        return if sprite_attrib.flip_y{
+            // Since Im flipping but dont know for what rect (8X8 or 8X16) I need sub this from the size (minus 1 casue im starting to count from 0 in the screen lines).
+            tile_num as u16 * 16 + (2 * (sprite_size - 1 - (16 - (sprite_attrib.y - ly_register)))) as u16
+        }
+        else{
+            // Since the sprite attribute y pos is the right most dot of the rect 
+            // Im subtracting this from 16 (since the rects are 8X16)
+            tile_num as u16 * 16 + (2 * (16 - (sprite_attrib.y - ly_register))) as u16
+        };
     }
 }
