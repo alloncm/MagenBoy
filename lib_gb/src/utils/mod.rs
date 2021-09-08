cc472ce5a24533e46225bbc464accfdad17e9b56
alloncm <alloncm@gmail.com>
2021-09-08 16:38:36 +0300

    Clean the create array util

--- lib_gb/src/utils/mod.rs
+++ lib_gb/src/utils/mod.rs
@@ -8,18 +8,9 @@ pub mod fixed_size_queue;
 pub const GB_FREQUENCY:u32 = 4_194_304;
 
 pub fn create_default_array<T:Default,const SIZE:usize>()->[T;SIZE]{
-    let mut data: [MaybeUninit<T>; SIZE] = unsafe{MaybeUninit::uninit().assume_init()};
-
-    for elem in &mut data[..]{
-        *elem = MaybeUninit::new(T::default());
-    }
-    unsafe{
-        let casted_data = std::ptr::read(&data as *const [MaybeUninit<T>;SIZE] as *const [T;SIZE]);
-        std::mem::forget(data);
-        // std::mem::forget(data);
-        return casted_data;
-    }
+    create_array(||T::default())
 }
+
 pub fn create_array<T, F:Fn()->T,const SIZE:usize>(func:F)->[T;SIZE]{
     let mut data: [MaybeUninit<T>; SIZE] = unsafe{MaybeUninit::uninit().assume_init()};
 
@@ -29,7 +20,6 @@ pub fn create_array<T, F:Fn()->T,const SIZE:usize>(func:F)->[T;SIZE]{
     unsafe{
         let casted_data = std::ptr::read(&data as *const [MaybeUninit<T>;SIZE] as *const [T;SIZE]);
         std::mem::forget(data);
-        // std::mem::forget(data);
         return casted_data;
     }
 }
