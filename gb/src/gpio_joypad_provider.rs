46d2f40e5fbc5324921bb2fa22744cc8308a6a61
alloncm <alloncm@gmail.com>
2022-01-14 17:05:43 +0200

    Fixed the gpio input

--- gb/src/gpio_joypad_provider.rs
+++ gb/src/gpio_joypad_provider.rs
@@ -3,6 +3,7 @@ use lib_gb::keypad::{
     joypad_provider::JoypadProvider,
     button::Button
 };
+use lib_gb::utils::create_default_array;
 use rppal::gpio::{
     Gpio,
     InputPin
@@ -23,13 +24,13 @@ impl GpioJoypadProvider{
         let right_pin = gpio.get(21).unwrap().into_input();
         let left_pin = gpio.get(19).unwrap().into_input();
         
-        let mut pins:[Option<InputPin>;NUM_OF_KEYS] = [None;NUM_OF_KEYS];
-        pins[Button::A as u8] = Some(a_pin);
-        pins[Button::B as u8] = Some(b_pin);
-        pins[Button::Up as u8] = Some(up_pin);
-        pins[Button::Down as u8] = Some(down_pin);
-        pins[Button::Right as u8] = Some(right_pin);
-        pins[Button::Left as u8] = Some(left_pin);
+        let mut pins:[Option<InputPin>;NUM_OF_KEYS] = create_default_array();
+        pins[Button::A as usize] = Some(a_pin);
+        pins[Button::B as usize] = Some(b_pin);
+        pins[Button::Up as usize] = Some(up_pin);
+        pins[Button::Down as usize] = Some(down_pin);
+        pins[Button::Right as usize] = Some(right_pin);
+        pins[Button::Left as usize] = Some(left_pin);
 
         Self{
             input_pins:pins
@@ -40,8 +41,8 @@ impl GpioJoypadProvider{
 impl JoypadProvider for GpioJoypadProvider{
     fn provide(&mut self, joypad:&mut Joypad){
         for i in 0..NUM_OF_KEYS{
-            if self.input_pins[i] = Some(pin){
-                joypad[i] = pin.is_high();
+            if let Some(pin) = &self.input_pins[i] {
+                joypad.buttons[i] = pin.is_high();
             }
         }
     }
