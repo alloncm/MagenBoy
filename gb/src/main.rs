1eb6d5cc81b080ed90cf31439515ecfe9ca993cf
Alon Cohen-Magen <alloncm@gmail.com>
2022-04-22 00:46:33 +0300

    Add rom_menu feature
    This feature pops a terminal menu to choose a rom from
    the rom folder supplied
    
    The idea is to allow the player to have one command to
    boot the emulator and than he can choose new rom each time

--- gb/src/main.rs
+++ gb/src/main.rs
@@ -3,9 +3,11 @@ mod sdl_joypad_provider;
 mod sdl_gfx_device;
 mod mpmc_gfx_device;
 mod audio;
+mod joypad_terminal_menu;
 
-use crate::{audio::{ChosenResampler, multi_device_audio::*, ResampledAudioDevice}, mbc_handler::*, mpmc_gfx_device::MpmcGfxDevice, sdl_joypad_provider::*};
+use crate::{audio::{ChosenResampler, multi_device_audio::*, ResampledAudioDevice}, mbc_handler::*, mpmc_gfx_device::MpmcGfxDevice, sdl_joypad_provider::*, joypad_terminal_menu::TerminalRawModeJoypadProvider};
 use lib_gb::{GB_FREQUENCY, apu::audio_device::*, keypad::button::Button, machine::gameboy::GameBoy, mmu::gb_mmu::BOOT_ROM_SIZE, ppu::{gb_ppu::{BUFFERS_NUMBER, SCREEN_HEIGHT, SCREEN_WIDTH}, gfx_device::GfxDevice}};
+use joypad_terminal_menu::{MenuOption, JoypadTerminalMenu};
 use std::{fs, env, result::Result, vec::Vec};
 use log::info;
 use sdl2::sys::*;
@@ -51,12 +53,57 @@ fn buttons_mapper(button:Button)->SDL_Scancode{
     }
 }
 
+fn menu_buttons_mapper(button:crossterm::event::KeyCode)->Option<Button>{
+    match button{
+        crossterm::event::KeyCode::Char('x') => Option::Some(Button::A),
+        crossterm::event::KeyCode::Up        => Option::Some(Button::Up),
+        crossterm::event::KeyCode::Down      => Option::Some(Button::Down),
+        _=> Option::None
+    }
+}
+
 fn check_for_terminal_feature_flag(args:&Vec::<String>, flag:&str)->bool{
     args.len() >= 3 && args.contains(&String::from(flag))
 }
 
+fn get_terminal_feature_flag_value(args:&Vec<String>, flag:&str, error_message:&str)->String{
+    let index = args.iter().position(|v| *v == String::from(flag)).unwrap();
+    return args.get(index + 1).expect(error_message).clone();
+}
+
+fn get_game_selection(roms_path:&str)->String{
+    let mut menu_options = Vec::new();
+    let dir_entries = std::fs::read_dir(roms_path).expect(std::format!("Error openning the roms directory: {}",roms_path).as_str());
+    for entry in dir_entries{
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let extension = path.as_path().extension().unwrap();
+        if extension == "gb"{
+            let filename = String::from(path.file_name().expect("Error should be a file").to_str().unwrap());
+            let option = MenuOption{value: path, prompt: filename};
+            menu_options.push(option);
+        }
+    }
+
+    let mut menu = JoypadTerminalMenu::new(menu_options);
+    let mut provider = TerminalRawModeJoypadProvider::new(menu_buttons_mapper);
+    let result = menu.get_menu_selection(&mut provider);
+    // Removing the file extenstion and casting to String
+    let result = String::from(result.with_extension("").to_str().unwrap());
+    
+    return result;
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();    
+    let args: Vec<String> = env::args().collect();  
+
+    let program_name = if check_for_terminal_feature_flag(&args, "--rom_menu"){
+        let roms_path = get_terminal_feature_flag_value(&args, "--rom_menu", "Error couldnt find the roms folder specified");
+        get_game_selection(roms_path.as_str())
+    }
+    else{
+        args[1].clone()
+    };
 
     let debug_level = check_for_terminal_feature_flag(&args, "--log");
     
@@ -71,7 +118,6 @@ fn main() {
     let (s,r) = crossbeam_channel::bounded(BUFFERS_NUMBER - 1);
     let mpmc_device = MpmcGfxDevice::new(s);
 
-    let program_name = args[1].clone();
 
     let mut running = true;
     // Casting to ptr cause you cant pass a raw ptr (*const/mut T) to another thread
@@ -119,8 +165,7 @@ fn emulation_thread_main(args: Vec<String>, program_name: String, spsc_gfx_devic
     let mut mbc = initialize_mbc(&program_name);
     let joypad_provider = SdlJoypadProvider::new(buttons_mapper);
     let bootrom_path = if check_for_terminal_feature_flag(&args, "--bootrom"){
-        let index = args.iter().position(|v| *v == String::from("--bootrom")).unwrap();
-        args.get(index + 1).expect("Error! you must specify a value for the --bootrom parameter").clone()
+        get_terminal_feature_flag_value(&args, "--bootrom", "Error! you must specify a value for the --bootrom parameter")
     }else{
         String::from("dmg_boot.bin")
     };
