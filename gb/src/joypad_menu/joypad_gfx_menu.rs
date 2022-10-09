7cbd786f60077898e9d9425429ff5b85f931be1a
alloncm <alloncm@gmail.com>
2022-10-10 02:17:00 +0300

    Add all ascii visible chars and menu scroll
    * Add all ascii chars between 32-126
    * Support menu rom folder with more than 18 entries
    * Made gfx menu the default!

--- gb/src/joypad_menu/joypad_gfx_menu.rs
+++ gb/src/joypad_menu/joypad_gfx_menu.rs
@@ -1,6 +1,6 @@
 use lib_gb::ppu::{gfx_device::*, gb_ppu::{SCREEN_HEIGHT, SCREEN_WIDTH}, colors::{BLACK, WHITE}};
 
-use super::{font::{self, GLYPH_HEIGHT, GLYPH_WIDTH}, MenuRenderer};
+use super::{font::*, MenuRenderer};
 
 pub struct GfxDeviceMenuRenderer<'a, GFX:GfxDevice>{
     device:&'a mut GFX
@@ -13,28 +13,38 @@ impl<'a, GFX: GfxDevice> GfxDeviceMenuRenderer<'a, GFX> {
 impl<'a, GFX: GfxDevice, T> MenuRenderer<T> for GfxDeviceMenuRenderer<'a, GFX>{
     fn render_menu(&mut self, menu:&Vec<super::MenuOption<T>>, selection:usize) {
         let mut frame_buffer = [0 as Pixel; SCREEN_HEIGHT * SCREEN_WIDTH];
+
+        // Calculate the range of the visible menu
+        let mut start_index = 0;
+        let mut end_index = std::cmp::min(menu.len(), SCREEN_HEIGHT / GLYPH_HEIGHT);
+        if selection >= end_index{
+            end_index = selection + 1;
+            start_index = end_index - (SCREEN_HEIGHT / GLYPH_HEIGHT);
+        }
+
         let mut frame_buffer_height_index = 0;
-        for option_index in 0..menu.len(){
+        for option_index in start_index..end_index{
             let prompt = &menu[option_index].prompt;
             let mut width_index = 0;
             for char in prompt.as_bytes(){
-                // TODO: add support for non alphabetical chars
-                if !char.is_ascii_alphabetic(){continue;}
-                // TODO: add support for lower case alphabetical chars
-                let char = char.to_ascii_uppercase();
-                let glyph = font::FONT_LUT[(char - 'A' as u8) as usize];
+                let glyph = FONT_LUT[(char - FONT_ASCII_START_INDEX) as usize];
                 for i in 0..GLYPH_HEIGHT{
                     for j in 0..GLYPH_WIDTH{
-                        let pixel = if selection == option_index{
-                            Pixel::from(if glyph[i * GLYPH_WIDTH + j] {BLACK}else{WHITE})
+                        let color = if selection == option_index{
+                            if glyph[i * GLYPH_WIDTH + j] {BLACK}else{WHITE}
                         }
                         else{
-                            Pixel::from(if glyph[i * GLYPH_WIDTH + j] {WHITE}else{BLACK})
+                            if glyph[i * GLYPH_WIDTH + j] {WHITE}else{BLACK}
                         };
-                        frame_buffer[(frame_buffer_height_index + i) * SCREEN_WIDTH + width_index + j] = pixel;
+                        frame_buffer[(frame_buffer_height_index + i) * SCREEN_WIDTH + width_index + j] = Pixel::from(color);
                     }
                 }
                 width_index += GLYPH_WIDTH;
+
+                // if the game name it too long cut it 
+                if width_index >= SCREEN_WIDTH{
+                    break;
+                }
             }
             frame_buffer_height_index += GLYPH_HEIGHT;
         }
