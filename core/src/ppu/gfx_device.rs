401bb3b0886ecf368c21215ec45e45ffe606c246
alloncm <alloncm@gmail.com>
2024-09-23 23:12:26 +0300

    Fix the ili9341 RGB mode
    Also fix some comments

--- core/src/ppu/gfx_device.rs
+++ core/src/ppu/gfx_device.rs
@@ -1,5 +1,6 @@
 use super::gb_ppu::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
+/// Pixel is in the format of RGB555 as in the gbdev docs which is low bits (Red) -> high bits (Blue)
 pub type Pixel = u16;
 
 pub trait GfxDevice{
--- sdl/src/sdl_gfx_device.rs
+++ sdl/src/sdl_gfx_device.rs
@@ -3,7 +3,7 @@ use sdl2::sys::*;
 use magenboy_core::{ppu::gb_ppu::{SCREEN_HEIGHT, SCREEN_WIDTH}, utils::vec2::Vec2, GfxDevice, Pixel};
 use super::utils::get_sdl_error_message;
 
-// The bit order is high -> low bit as opposed to RGB555 in the gbdev docs which is low -> high bit
+// The bit order is high bits -> low bits as opposed to RGB555 in the gbdev docs which is low -> high 
 const SDL_PIXEL_FORMAT:u32 = SDL_PixelFormatEnum::SDL_PIXELFORMAT_BGR555 as u32;
 
 struct SdlWindow{
