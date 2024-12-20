096a67958b3e6d02452941b5cd2a849d943dfdcf
alloncm <alloncm@gmail.com>
2024-12-20 13:12:28 +0200

    test

--- common/build.rs
+++ common/build.rs
@@ -1,12 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let git_path = std::path::Path::new("../.git");
-    let head_path = git_path.join("HEAD");
-    let current_branch = std::fs::read_to_string(&head_path).unwrap().replace("refs: ", "");
-    let current_commit_file = git_path.join(&current_branch);
-    println!("cargo:rerun-if-changed={}", head_path.to_str().unwrap());
-    println!("cargo:rerun-if-changed={}", current_commit_file.to_str().unwrap());
+    // let git_path = std::path::Path::new("../.git");
+    // let head_path = git_path.join("HEAD");
+    // let current_branch = std::fs::read_to_string(&head_path).unwrap().replace("refs: ", "");
+    // let current_commit_file = git_path.join(&current_branch);
+    // println!("cargo:rerun-if-changed={}", head_path.to_str().unwrap());
+    // println!("cargo:rerun-if-changed={}", current_commit_file.to_str().unwrap());
 
     // let output = Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output().unwrap();
     // let git_hash = String::from_utf8(output.stdout).unwrap();
