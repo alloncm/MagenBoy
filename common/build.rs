d43594df724074c1ac873562c53f6b719ff946cf
alloncm <alloncm@gmail.com>
2024-12-20 13:16:27 +0200

    one more test

--- common/build.rs
+++ common/build.rs
@@ -1,11 +1,11 @@
 use std::process::Command;
 
 fn main() {
-    // let git_path = std::path::Path::new("../.git");
-    // let head_path = git_path.join("HEAD");
+    let git_path = std::path::Path::new("../.git");
+    let head_path = git_path.join("HEAD");
     // let current_branch = std::fs::read_to_string(&head_path).unwrap().replace("refs: ", "");
     // let current_commit_file = git_path.join(&current_branch);
-    // println!("cargo:rerun-if-changed={}", head_path.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}", head_path.to_str().unwrap());
     // println!("cargo:rerun-if-changed={}", current_commit_file.to_str().unwrap());
 
     let output = Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output().unwrap();
