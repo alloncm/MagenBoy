338a79a50b5bf7acdc2ebee5bf42148d2efb49e6
alloncm <alloncm@gmail.com>
2025-10-12 00:19:35 +0300

    Clean gl impl

--- gl/src/render.rs
+++ gl/src/render.rs
@@ -14,14 +14,26 @@ pub struct GlRenderer {
     vertex_array_object: GLuint,
     vertex_buffer_object: GLuint,
     element_buffer_object: GLuint,
+    texture_object: GLuint,
 }
 
-const POS_TEX_VERTICES: [f32; 16] = [
-    1.0, 1.0, 1.0, 0.0,     // top right
-    1.0, -1.0, 1.0, 1.0,    // bottom right
-    -1.0, -1.0, 0.0, 1.0,   // bottom left
-    -1.0, 1.0, 0.0, 0.0     // top left
-]; 
+#[repr(C, packed)]
+#[derive(Clone, Copy)]
+struct Vertex {
+    pos_x: GLfloat,
+    pos_y: GLfloat,
+    tex_x: GLfloat,
+    tex_y: GLfloat
+}
+
+// texture coordinates are flipped vertically since OpenGL's texture origin is bottom-left
+const POS_TEX_VERTICES: [Vertex; 4] = [
+    Vertex { pos_x: 1.0, pos_y: 1.0, tex_x: 1.0, tex_y: 0.0 },     // top right
+    Vertex { pos_x: 1.0, pos_y: -1.0, tex_x: 1.0, tex_y: 1.0 },    // bottom right
+    Vertex { pos_x: -1.0, pos_y: -1.0, tex_x: 0.0, tex_y: 1.0 },   // bottom left
+    Vertex { pos_x: -1.0, pos_y: 1.0, tex_x: 0.0, tex_y: 0.0 }     // top left
+];
+
 const INDICIES: [u32; 6] = [
     0, 1, 3, // first triangle
     1, 2, 3  // second triangle
@@ -30,47 +42,7 @@ const INDICIES: [u32; 6] = [
 impl GlRenderer{
     pub fn new(window: *mut GLFWwindow) -> GlRenderer{
         unsafe {
-            // build and compile our shader program
-            // ------------------------------------
-            // vertex shader
-            let vertex_shader_source = CString::new(VERTEX_SHADER_SOURCE).unwrap();
-            let vertex_shader: GLuint = gl::CreateShader(gl::VERTEX_SHADER);
-            gl::ShaderSource(vertex_shader, 1, &vertex_shader_source.as_ptr(), null());
-            gl::CompileShader(vertex_shader);
-            // check for shader compile errors
-            let mut success:GLint = 0;
-            let mut info_log:[GLchar; 512] = [0; 512];
-            gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success as *mut GLint);
-            if success == 0 {
-                gl::GetShaderInfoLog(vertex_shader, info_log.len() as GLint, null_mut(), info_log.as_mut_ptr());
-                let info_log = CStr::from_ptr(info_log.as_ptr());
-                println!("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n {:?}", info_log);
-            }
-            // fragment shader
-            let fragment_shader: GLuint = gl::CreateShader(gl::FRAGMENT_SHADER);
-            let fragment_shader_source = CString::new(FRAGMENT_SHADER_SOURCE).unwrap();
-            gl::ShaderSource(fragment_shader, 1, &fragment_shader_source.as_ptr(), null());
-            gl::CompileShader(fragment_shader);
-            // check for shader compile errors
-            gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success as *mut GLint);
-            if success == 0 {
-                gl::GetShaderInfoLog(fragment_shader, 512, null_mut(), info_log.as_mut_ptr());
-                let info_log = CStr::from_ptr(info_log.as_ptr());
-                println!("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n {:?}", info_log);
-            }
-            // link shaders
-            let shader_program: GLuint = gl::CreateProgram();
-            gl::AttachShader(shader_program, vertex_shader);
-            gl::AttachShader(shader_program, fragment_shader);
-            gl::LinkProgram(shader_program);
-            // check for linking errors
-            gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success as *mut GLint);
-            if success == 0 {
-                gl::GetProgramInfoLog(shader_program, info_log.len() as GLint,null_mut(), info_log.as_mut_ptr());
-                println!("ERROR::SHADER::PROGRAM::LINKING_FAILED\n {:?}", info_log);
-            }
-            gl::DeleteShader(vertex_shader);
-            gl::DeleteShader(fragment_shader);
+            let shader_program = Self::link_shader_program();
 
             let mut vertex_buffer_object: GLuint = 0;
             let mut vertex_array_object: GLuint = 0;
@@ -87,7 +59,7 @@ impl GlRenderer{
             gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, element_buffer_object);
             gl::BufferData(gl::ELEMENT_ARRAY_BUFFER, std::mem::size_of_val(&INDICIES) as GLsizeiptr, INDICIES.as_ptr() as *const _, gl::STATIC_DRAW);
 
-            let stride = (4 * std::mem::size_of::<GLfloat>()) as GLint;
+            let stride = std::mem::size_of::<Vertex>() as GLint;
             // pos attribute
             gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, stride, 0 as *const _);
             gl::EnableVertexAttribArray(0);
@@ -95,45 +67,15 @@ impl GlRenderer{
             gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, stride, (2 * size_of::<GLfloat>()) as *const _);
             gl::EnableVertexAttribArray(1);
 
-            // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-            gl::BindBuffer(gl::ARRAY_BUFFER, 0); 
-
-            let mut texture: GLuint = 0;
-            gl::GenTextures(1, &mut texture as *mut _);
-            gl::BindTexture(gl::TEXTURE_2D, texture);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_BORDER as GLint);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_BORDER as GLint);
-            // Nearest upscaling instead of linear
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as GLint);
-            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as GLint);
-            gl::TexImage2D(
-                gl::TEXTURE_2D, 
-                0, 
-                gl::RGB as _, 
-                SCREEN_WIDTH as _, 
-                SCREEN_HEIGHT as _, 
-                0, 
-                gl::RGB, 
-                gl::UNSIGNED_SHORT_5_6_5, 
-                null()
-            );
-            if let Err(e) = check_gl_error(){
-                panic!("Error creating texture: {}", e);
-            }
+            let texture_object = Self::allocate_texture_object();
 
             gl::UseProgram(shader_program);
 
-            // Probably not required on all hardware
-            let texture_name = CString::new("Texture").unwrap();
-            let uniform_location = gl::GetUniformLocation(shader_program, texture_name.as_ptr());
-            gl::Uniform1i(uniform_location, 0);
-            gl::ActiveTexture(gl::TEXTURE0);
-            gl::BindTexture(gl::TEXTURE_2D, texture);
-
-            let (mut width, mut height) = (0, 0);
-            glfwGetFramebufferSize(window, &mut width, &mut height);
+            Self::bind_uniform_texture(shader_program);
 
             // Set initial viewport
+            let (mut width, mut height) = (0, 0);
+            glfwGetFramebufferSize(window, &mut width, &mut height);
             update_viewport_callback(window, width, height);
 
             return GlRenderer {
@@ -142,6 +84,7 @@ impl GlRenderer{
                 vertex_array_object,
                 vertex_buffer_object,
                 element_buffer_object,
+                texture_object
             };
         }
     }
@@ -169,34 +112,122 @@ impl GlRenderer{
             glfwSwapBuffers(self.window);
         }
     }
-}
 
-fn check_gl_error() -> Result<(), &'static str> {
-    let error = unsafe{gl::GetError()};
-    if error != gl::NO_ERROR {
-        return Err(match error {
-            gl::INVALID_ENUM => "GL_INVALID_ENUM",
-            gl::INVALID_VALUE => "GL_INVALID_VALUE",
-            gl::INVALID_OPERATION => "GL_INVALID_OPERATION",
-            gl::STACK_OVERFLOW => "GL_STACK_OVERFLOW",
-            gl::STACK_UNDERFLOW => "GL_STACK_UNDERFLOW",
-            gl::OUT_OF_MEMORY => "GL_OUT_OF_MEMORY",
-            gl::INVALID_FRAMEBUFFER_OPERATION => "GL_INVALID_FRAMEBUFFER_OPERATION",
-            _ => "Unknown error",
-        });
+    unsafe fn link_shader_program() -> u32 {
+        // vertex shader
+        let vertex_shader_source = CString::new(VERTEX_SHADER_SOURCE).unwrap();
+        let vertex_shader: GLuint = gl::CreateShader(gl::VERTEX_SHADER);
+        gl::ShaderSource(vertex_shader, 1, &vertex_shader_source.as_ptr(), null());
+        gl::CompileShader(vertex_shader);
+        Self::veify_shader_compile_status(vertex_shader);
+
+        // fragment shader
+        let fragment_shader: GLuint = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let fragment_shader_source = CString::new(FRAGMENT_SHADER_SOURCE).unwrap();
+        gl::ShaderSource(fragment_shader, 1, &fragment_shader_source.as_ptr(), null());
+        gl::CompileShader(fragment_shader);
+        Self::veify_shader_compile_status(fragment_shader);
+
+        // link shaders
+        let shader_program: GLuint = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+        // check for linking errors
+        Self::veify_shader_program_link_status(shader_program);
+
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+        shader_program
     }
 
-    return Ok(());
+    unsafe fn veify_shader_compile_status(shader: u32) {
+        let mut success:GLint = 0;
+        let mut info_log:[GLchar; 512] = [0; 512];
+
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success as *mut GLint);
+        if success == 0 {
+            gl::GetShaderInfoLog(shader, info_log.len() as GLint, null_mut(), info_log.as_mut_ptr());
+            let info_log = CStr::from_ptr(info_log.as_ptr());
+            std::panic!("shader compilation failed {:?}", info_log);
+        }
+    }
+
+    unsafe fn veify_shader_program_link_status(program: u32) {
+        let mut success:GLint = 0;
+        let mut info_log:[GLchar; 512] = [0; 512];
+
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut success as *mut GLint);
+        if success == 0 {
+            gl::GetProgramInfoLog(program, info_log.len() as GLint,null_mut(), info_log.as_mut_ptr());
+            let info_log = CStr::from_ptr(info_log.as_ptr());
+            std::panic!("shader program link failed {:?}", info_log);
+        }
+    }
+
+    fn check_gl_error() -> Result<(), &'static str> {
+        let error = unsafe{gl::GetError()};
+        if error != gl::NO_ERROR {
+            return Err(match error {
+                gl::INVALID_ENUM => "GL_INVALID_ENUM",
+                gl::INVALID_VALUE => "GL_INVALID_VALUE",
+                gl::INVALID_OPERATION => "GL_INVALID_OPERATION",
+                gl::STACK_OVERFLOW => "GL_STACK_OVERFLOW",
+                gl::STACK_UNDERFLOW => "GL_STACK_UNDERFLOW",
+                gl::OUT_OF_MEMORY => "GL_OUT_OF_MEMORY",
+                gl::INVALID_FRAMEBUFFER_OPERATION => "GL_INVALID_FRAMEBUFFER_OPERATION",
+                _ => "Unknown error",
+            });
+        }
+
+        return Ok(());
+    }
+
+    unsafe fn allocate_texture_object() -> u32 {
+        let mut texture_object: GLuint = 0;
+        gl::GenTextures(1, &mut texture_object as *mut _);
+        gl::BindTexture(gl::TEXTURE_2D, texture_object);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_BORDER as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_BORDER as GLint);
+
+        // Nearest upscaling, those must be set for the texture to render
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as GLint);
+
+        gl::TexImage2D(
+            gl::TEXTURE_2D, 
+            0, 
+            gl::RGB as _, 
+            SCREEN_WIDTH as _, 
+            SCREEN_HEIGHT as _, 
+            0, 
+            gl::RGB, 
+            gl::UNSIGNED_SHORT_5_6_5, 
+            null()
+        );
+        if let Err(e) = Self::check_gl_error(){
+            panic!("Error creating texture: {}", e);
+        }
+        return texture_object;
+    }
+
+    /// shader program must be active.
+    /// Since we have only single texture uniform OpenGL will auto bind it on most hardware
+    unsafe fn bind_uniform_texture(shader_program: u32) {
+        let texture_name = CString::new("Texture").unwrap();
+        let uniform_location = gl::GetUniformLocation(shader_program, texture_name.as_ptr());
+        gl::Uniform1i(uniform_location, 0);
+        gl::ActiveTexture(gl::TEXTURE0);
+    }
 }
 
 impl Drop for GlRenderer {
     fn drop(&mut self) {
         unsafe {
-            // optional: de-allocate all resources once they've outlived their purpose:
-            // ------------------------------------------------------------------------
             gl::DeleteVertexArrays(1, &self.vertex_array_object);
             gl::DeleteBuffers(1, &self.vertex_buffer_object);
             gl::DeleteBuffers(1, &self.element_buffer_object);
+            gl::DeleteTextures(1, &self.texture_object);
             gl::DeleteProgram(self.shader_program);
         }
     }
